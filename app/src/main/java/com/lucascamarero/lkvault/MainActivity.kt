b8a87b258dd275cb65a8b8dd1b821bc81a9abac5
Lucas Camarero <luken@gmx.es>
2026-05-12 12:51:11 +0200

    feat / commit 40 / Main activity con splash screen activado

--- app/src/main/java/com/lucascamarero/lkvault/MainActivity.kt
+++ app/src/main/java/com/lucascamarero/lkvault/MainActivity.kt
@@ -46,7 +46,7 @@ class MainActivity : AppCompatActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-                ) {/*
+                ) {
                     when {
 
                         // Pantalla inicial de presentación (Splash)
@@ -62,11 +62,7 @@ class MainActivity : AppCompatActivity() {
                                 languageViewModel = languageViewModel
                             )
                         }
-                    }*/
-                    ScreenManager(
-                        languageViewModel = languageViewModel
-                    )
-
+                    }
                 }
             }
         }
